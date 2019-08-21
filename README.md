af6863e4e4ef7df58927084dce04cafc734d264a
Kristoffer <hello@kristoffer.is>
2019-08-21 09:29:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# @denkristoffer/eslint-config
+
+## Install
+
+Install the package
+
+```sh
+yarn add -D eslint @denkristoffer/eslint-config
+```
+
+and add it to your `.eslintrc.json`:
+
+```json
+{
+  "root": true,
+  "extends": ["@denkristoffer/eslint-config"]
+}
+```
---
+++ src/index.js
@@ -0,0 +1,94 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/recommended',
+    'plugin:unicorn/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/unicorn',
+  ],
+  overrides: [
+    {
+      files: ['**/*.tsx'],
+      rules: {
+        'no-null/no-null': 'off',
+        'unicorn/prevent-abbreviations': [
+          'error',
+          {
+            replacements: {
+              props: false,
+              ref: false,
+            },
+          },
+        ],
+      },
+    },
+    {
+      files: ['**/jest.config.js'],
+      rules: {
+        'unicorn/prevent-abbreviations': [
+          'error',
+          {
+            whitelist: {
+              setupFilesAfterEnv: true,
+            },
+          },
+        ],
+      },
+    },
+    {
+      files: ['**/next.config.js'],
+      rules: {
+        'unicorn/prevent-abbreviations': [
+          'error',
+          {
+            whitelist: {
+              distDir: true,
+            },
+          },
+        ],
+      },
+    },
+    {
+      files: ['**/__tests__/**/*'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'jsx-a11y',
+    'no-null',
+    'prettier',
+    'react',
+    'react-hooks',
+    'unicorn',
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': [
+      'warn',
+      {
+        allowExpressions: true,
+        allowHigherOrderFunctions: true,
+        allowTypedFunctionExpressions: true,
+      },
+    ],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-null/no-null': 'error',
+    'no-var': 'warn',
+    'react/prop-types': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'unicorn/filename-case': ['error', { case: 'camelCase' }],
+    'unicorn/prevent-abbreviations': [
+      'error',
+      { replacements: { args: false } },
+    ],
+  },
+}
